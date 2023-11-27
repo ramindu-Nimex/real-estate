15eb04508e5daa83746c79c1214ae392df713c3a
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2023-11-27 23:23:21 +0530

    create sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
+// SIGN UP
 export const signup = async(req,res, next) => {
    const {username,email,password} = req.body;
    const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -10,6 +13,22 @@ export const signup = async(req,res, next) => {
       await newUser.save();
       res.status(201).json('user Created successfully!');
    } catch(error) {
+      next(error);
+   }
+}
+
+// SIGN IN
+export const signin = async(req,res, next) => {
+   const {email,password} = req.body;
+   try {
+      const validUser = await User.findOne({email});
+      if(!validUser) return next(errorHandler(404, 'User not found!'));
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+      const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+      const {password: pass, ...rest} = validUser._doc;
+      res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+   } catch (error) {
       next(error);
    }
 }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import expree from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup,signin } from '../controllers/auth.controller.js';
 
 const router = expree.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
