793d7d9e45ade3c4c96417cca7b0e412b0f044e3
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2023-11-29 15:03:10 +0530

    complete create listing ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
+import CreateListing from "./pages/createListing"
 import PrivateRoute from "./components/PrivateRoute"
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/about" element={<About/>} />
         <Route element={<PrivateRoute/>}>
           <Route path="/profile" element={<Profile/>} />
+          <Route path="/create-listing" element={<CreateListing/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useRef, useState, useEffect } from 'react'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase.js'
 import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signOutUserStart, signOutUserSuccess, signOutUserFailure } from '../redux/user/userSlice.js'
-
+import {Link} from 'react-router-dom'
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -123,6 +123,9 @@ export default function Profile() {
         <input type="email" placeholder='E-MAIL' className='border p-3 rounded-lg' defaultValue={currentUser.email} id='email' onChange={handleChange}/>
         <input type="password" placeholder='PASSWORD' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'LOADING...' : 'UPDATE'}</button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95 disabled:opacity-80' to={"/create-listing"}>
+          CREATE LISTING
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
