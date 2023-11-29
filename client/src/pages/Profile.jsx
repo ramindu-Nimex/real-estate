26390b4218db0acebbb5c1d3bb218c266e9dbb1b
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2023-11-29 10:50:49 +0530

    add signout functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import {useSelector, useDispatch} from 'react-redux'
 import { useRef, useState, useEffect } from 'react'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase.js'
-import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure} from '../redux/user/userSlice.js'
+import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signOutUserStart, signOutUserSuccess, signOutUserFailure } from '../redux/user/userSlice.js'
 
 
 export default function Profile() {
@@ -93,6 +93,21 @@ export default function Profile() {
     }
   }
 
+  const handleSignOut = async() => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/server/auth/signout');
+      const data = await res.json();
+      if(data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7 uppercase'>Profile</h1>
@@ -111,7 +126,7 @@ export default function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully!' : ''}</p>
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,11 @@
 import expree from 'express';
-import { signup,signin,google } from '../controllers/auth.controller.js';
+import { signup,signin,google,signout } from '../controllers/auth.controller.js';
 
 const router = expree.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signout);
 
 export default router;
