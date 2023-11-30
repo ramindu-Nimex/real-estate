202e69a9603888dabcc982a82824d931b348fa65
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2023-11-30 23:54:37 +0530

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -17,7 +17,6 @@ export default function Search() {
    const [loading, setLoading] = useState(false);
    const [listings, setListings] = useState([]);
    const [showMore, setShowMore] = useState(false);
-   console.log(listings);
 
    useEffect(() => {
       const urlParams = new URLSearchParams(location.search);
@@ -97,6 +96,20 @@ export default function Search() {
       navigate(`/search?${searchQuery}`);
    }
 
+   const onShowMoreClick = async() => {
+      const numberOfListings = listings.length;
+      const startIndex = numberOfListings;
+      const urlParams = new URLSearchParams(location.search);
+      urlParams.set('startIndex', startIndex);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/server/listing/get?${searchQuery}`);
+      const data = await res.json();
+      if (data.length < 9) {
+         setShowMore(false);
+      }
+      setListings([...listings, ...data]);
+   }
+
   return (
     <div className='flex flex-col md:flex-row'>
       <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
@@ -159,7 +172,7 @@ export default function Search() {
             {!loading && listings && listings.map((listing) => (
                <Listingitem key={listing._id} listing={listing} />
             ))}
-            
+            {showMore && (<button onClick={onShowMoreClick} className='text-green-700 hover:underline p-7 text-center w-full'>Show More</button>)}
          </div>
       </div>
     </div>
